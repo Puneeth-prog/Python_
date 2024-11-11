7cf55ccec59ed8e2f41aaf2c92c6a9df224b58f0
Puneeth-prog <puneethjay619@gmail.com>
2024-11-11 13:30:01 +0530

    pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,6 +1,6 @@
-rows = int(input("Enter number of rows: "))
+rows = 11  #number rows required
 
-k = 0
+k = 0                                         # initializing the column to 0
 
 for i in range(1, rows + 1):
     for space in range(1, (rows - i) + 1):
@@ -8,7 +8,7 @@ for i in range(1, rows + 1):
 
     while k != (2 * i - 1):
         print(k , end=" ")
-        k += 1
+        k += 1                                 #increase the number count
 
-    k = 0
+    k = 0                                      #again start printing from 0
     print()
